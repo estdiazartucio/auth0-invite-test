012b6fe177962e4bba8da9548433133da3b04bdf
Estefania Diaz Artucio <ediaz@quadiontech.com>
2019-02-26 18:11:27 -0300

    Added homepage component and react-router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,15 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
-import logo from './common/assets/iconfinder_octocat.svg';
-import './App.css';
+import {Route, BrowserRouter, Switch} from 'react-router-dom';
+import Home from './Homepage/Home.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+            <Switch>
+                <Route path='/' component={Home}/>
+            </Switch>
+      </BrowserRouter>
     );
   }
 }
