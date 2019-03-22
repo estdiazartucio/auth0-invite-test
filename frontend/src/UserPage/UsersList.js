8c19a7934a2b04a71775eed71a7354b226c677a8
Estefania Diaz Artucio <ediaz@quadiontech.com>
2019-03-22 14:48:31 -0300

    Fixed some more styles. Made users page responsive

--- frontend/src/UserPage/UsersList.js
+++ frontend/src/UserPage/UsersList.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import defaultIcon from '.././common/assets/user_circle.svg';
 import './UserPage.css';
 
 const UsersList = props => {
     const renderRows = () => {
         return props.users.map(user => 
             <div className='user-list-row' key={user.id}>
-                {user.email}
+                <span className='user-list-column'><img src={defaultIcon} alt='userIcon' /></span>
+                <span className='user-list-column'>{user.email}</span>
+                <span className='user-list-column'>{user.email}</span>
             </div>
         )
     }
@@ -13,7 +16,9 @@ const UsersList = props => {
     return(
         <div className='user-list-container'>
             <div className='user-list-row'>
-                Email
+                <span className='user-list-column'></span>
+                <span className='user-list-column'>Email</span>
+                <span className='user-list-column'>Username</span>
             </div>
             {props.users && props.users.length > 0 ? renderRows() : <div className='user-list-row'>No users found</div>}
         </div>
